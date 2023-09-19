b3200df5eb862bea6ca4130a4c1b17a38f53a1dd
Gogs <gogs@fake.local>
2023-09-19 18:06:37 +0200

    19_01

--- app/src/main/java/com/example/sixteendays/MainActivity.kt
+++ app/src/main/java/com/example/sixteendays/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid=443c1c3e63cf5d70eee6cd4cb67513e9
     }
 }
